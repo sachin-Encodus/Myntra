5772913e4cc97442eec23cb5df34971f59cdc763
rohitparashar123 <parasharjirohit@gmail.com>
2022-03-04 18:46:23 +0530

    footer and navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='container'>
+    <nav>
+      <input type="checkbox" id="check" />
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">Myntra</label>
+      <ul>
+        <li><a class="active" href="#">Home</a></li>
+        <li><a href="#">Blogs</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <div className='search'>
+          <input type="search" name="" id="" />
+          <a href="#" className='submit'>
+
+          </a>
+      </div>
+    </nav>
+  </header>
   )
 }
 
