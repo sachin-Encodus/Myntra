8600448a8b9b88338ffe8e61572dd37ea114d8cf
rohitparashar123 <parasharjirohit@gmail.com>
2022-03-05 19:15:12 +0530

    Details page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
+import DetailScreen from './screens/DetailScreen';
 
 function App() {
   return (
     <div>
       <HomeScreen />
+      {/* <DetailScreen /> */}
     </div>
   );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/navbar/Navbar';
+import Bag from '../components/bag/Bag';
 import Footer from '../components/footer/Footer';
 
 const HomeScreen = () => {
   return (
     <div>
         <Navbar />
+        <Bag />
         <Footer />
     </div>
   )
