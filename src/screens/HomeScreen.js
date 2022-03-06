87dd52572cd65fa2b83e26b3c63cdd0d60312bf2
rohitparashar123 <parasharjirohit@gmail.com>
2022-03-06 12:25:58 +0530

    main page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from '../components/navbar/Navbar';
-import Bag from '../components/bag/Bag';
+// import Bag from '../components/bag/Bag';
+import Main from '../components/main/Main';
 import Footer from '../components/footer/Footer';
 
 const HomeScreen = () => {
   return (
     <div>
         <Navbar />
-        <Bag />
+        {/* <Bag /> */}
+        <Main />
         <Footer />
     </div>
   )
